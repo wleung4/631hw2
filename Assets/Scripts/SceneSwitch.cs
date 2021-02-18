064c6dea950729c8c9b2f53eaf7c1aa3b7722c12
wleung4 <leungweyman@gmail.com>
2021-02-18 00:27:39 -0800

    hw2
    attempt to push

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void SwitchToScene1(){
+        SceneManager.LoadScene("Scene 1");
+    }
+
+    public void switchToScene2(){
+        SceneManager.LoadScene("Scene 2");
+    }
+}
